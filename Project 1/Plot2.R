a899d0fd1ee2d710e5461a5fa512c37a190a1367
VictorT <victortrn@gmail.com>
2015-07-12 22:49:46 +0300

    First commit

---
+++ Project 1/Plot2.R
@@ -0,0 +1,5 @@
+datetime <- strptime(paste(subData$Date, subData$Time, sep=" "), "%d/%m/%Y %H:%M:%S") 
+globalActivePower <- as.numeric(subData$Global_active_power)
+png("plot2.png", width=480, height=480)
+plot(datetime, globalActivePower, type="l", xlab="", ylab="Global Active Power (kilowatts)")
+dev.off()
